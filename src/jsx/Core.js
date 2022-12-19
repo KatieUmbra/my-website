980440615d82a8dcbcb474d7ff313e8816455a22
KanwiNeko <business@kanwi.gay>
2022-12-18 23:19:19 -0500

    First update

--- src/jsx/Core.js
+++ src/jsx/Core.js
@@ -1,10 +1,17 @@
 import "./css/Core.css";
+import "./js/app.js"
 function App() {
   return (
-    <div className="h-screen flex justify-center items-center flex-col gap-y-6 flex-wrap dark:bg-gray-900 bg-white">
-      <h1 className="Header">Welcome to Reactbreeze! 👋</h1>
-      <p className="Text">Reactbreeze simplifies the setup of React-based Tailwind applications by having an organized file structure with all the tools that you need, all out-of-the-box. <b>Work more efficiently with Reactbreeze.</b></p>
-      <a target="_blank" rel="noreferrer noopener" href="https://github.com/hydrabank/cra-template-reactbreeze.git"><button className="Button">Learn more</button></a>
+    <div class="main">
+      <div class="terminal">
+          <div class="tabs">
+            <div class="tab">
+            </div>
+          </div>
+          <div class="terminalField">
+
+          </div>
+      </div>
     </div>
   );
 }
